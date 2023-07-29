9f9d1beb66e8ed13f71ab3297d2d3e9721d0ac98
ThanhDuong <dngtrnthanh@gmail.com>
2023-07-30 00:26:29 +0700

    [update] update settings and urls

---
+++ lib_system/admin_page/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('dashboard/', views.dashBoard, name='dashboard'),
+    path('books/', views.books, name='books')
+]
--- lib_system/lib_system/settings.py
+++ lib_system/lib_system/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin_page',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
