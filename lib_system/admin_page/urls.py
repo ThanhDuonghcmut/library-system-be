de19aeb73ca38dca5dbc0da2835693efadb54c36
ThanhDuong <dngtrnthanh@gmail.com>
2023-07-31 16:33:21 +0700

    [add] add /category

--- lib_system/admin_page/urls.py
+++ lib_system/admin_page/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashBoard, name='dashboard'),
-    path('books/', views.books, name='books')
+    path('books/', views.books, name='books'),
+    path('category/', views.category, name='category')
 ]
